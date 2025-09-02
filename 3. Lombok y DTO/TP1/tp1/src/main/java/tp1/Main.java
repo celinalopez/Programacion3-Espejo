f2b681611f1e0a3e44a2bada3635b23af64b5553
celinalopez <celina.lopez2799@gmail.com>
2025-09-02 00:12:38 -0300

    Lombok tp1

---
+++ 3. Lombok y DTO/TP1/tp1/src/main/java/tp1/Main.java
@@ -0,0 +1,16 @@
+package tp1;
+public class Main {
+    public static void main(String[] args) {
+        // Usando constructor vacío
+        Persona persona1 = new Persona();
+        persona1.setNombre("Ana");
+        persona1.setEdad(25);
+
+        System.out.println("Persona 1: " + persona1.getNombre() + ", Edad: " + persona1.getEdad());
+
+        // Usando constructor con parámetros
+        Persona persona2 = new Persona("Juan", 30);
+
+        System.out.println("Persona 2: " + persona2.getNombre() + ", Edad: " + persona2.getEdad());
+    }
+}
---
+++ 3. Lombok y DTO/TP1/tp1/src/main/java/tp1/Persona.java
@@ -0,0 +1,15 @@
+package tp1;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor       // Constructor vacío
+@AllArgsConstructor     // Constructor con todos los atributos
+public class Persona {
+    private String nombre;
+    private int edad;
+}
+
